93850f9e0cb3f9f4ded01e60565d7af5ef6b5242
Camille MacLaren <camillemaclaren@gmail.com>
2023-07-18 01:05:12 -0500

    Update index.js
    put questions in question array
    add license prompt

--- index.js
+++ index.js
@@ -4,7 +4,47 @@ const fs = require('fs');
 const markdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
-    
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'What is it\'s description?',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'Installation instructions?',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'What is it\'s usage?',
+        name: 'usage'
+      },
+      {
+        type: 'list',
+        message: 'what is it\s license?',
+        choices: ['MIT License', 'GNU'],
+        name: 'license'
+      },
+      {
+        type: 'input',
+        message: 'Enter Test insrtuctions',
+        name: 'tests'
+      },
+      {
+        type: 'input',
+        message: 'Enter GitHub username',
+        name: 'username'
+      },
+      {
+        type: 'input',
+        message: 'Enter email',
+        name: 'email'
+      }
 ];
 
 // TODO: Create a function to write README file
@@ -13,34 +53,7 @@ function writeToFile(fileName, data) {}
 // TODO: Create a function to initialize app
 function init() {
 
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is the title of your project?',
-            name: 'title',
-          },
-          {
-            type: 'input',
-            message: 'What is it\'s description?',
-            name: 'description',
-          },
-          {
-            type: 'input',
-            message: 'Installation instructions?',
-            name: 'installation',
-          },
-          {
-            type: 'input',
-            message: 'What is it\'s usage?',
-            name: 'usage'
-          },
-          {
-            type: 'input',
-            message: 'How can you test it?',
-            name: 'tests'
-          }
-    
-    ])
+    inquirer.prompt(questions)
 }
 
 // Function call to initialize app
