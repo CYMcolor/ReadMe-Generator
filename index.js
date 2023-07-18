fa9063957472155787806c48b98293b1d39c34d5
Camille MacLaren <camillemaclaren@gmail.com>
2023-07-18 01:16:48 -0500

    start of writeToFile() function

--- index.js
+++ index.js
@@ -47,13 +47,19 @@ const questions = [
       }
 ];
 
+const fileName = 'test.txt';
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, JSON.stringify(data.title), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() {
 
     inquirer.prompt(questions)
+    .then((data) => {writeToFile(fileName, data)});
 }
 
 // Function call to initialize app
