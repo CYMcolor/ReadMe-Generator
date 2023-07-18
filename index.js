381e4e3cc4f2d7829ab422ac1e4cedd53f86aa5c
Camille MacLaren <camillemaclaren@gmail.com>
2023-07-18 00:36:25 -0500

    Setup
    init npm package info
    install version 2.8.4 of inquirer
    put guidelines of README as a placeholder
    init modules being used at beginning of of index.js
    start of inquirer prompts

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+          },
+          {
+            type: 'input',
+            message: 'What is it\'s description?',
+            name: 'description',
+          },
+          {
+            type: 'input',
+            message: 'Installation instructions?',
+            name: 'installation',
+          },
+          {
+            type: 'input',
+            message: 'What is it\'s usage?',
+            name: 'usage'
+          },
+          {
+            type: 'input',
+            message: 'How can you test it?',
+            name: 'tests'
+          }
+    
+    ])
+}
 
 // Function call to initialize app
 init();
